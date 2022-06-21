970772f8431630b42cdb4d1f2a708586210aff88
muhfatih <muhammadfatih4994@gmail.com>
2022-06-21 10:05:59 +0700

    wishlist

--- src/main/java/com/AltGame/AltGame/Controller/WishlistController.java
+++ src/main/java/com/AltGame/AltGame/Controller/WishlistController.java
@@ -1,30 +1,49 @@
 package com.AltGame.AltGame.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class WishlistController {
-    // Get All Data From Table
-    public void index()
-    {
+import com.AltGame.AltGame.Dto.ResponseDto;
+import com.AltGame.AltGame.Dto.WishlistDto;
+import com.AltGame.AltGame.Entity.WishlistEntity;
+import com.AltGame.AltGame.Service.WishlistService;
 
+@RestController
+@RequestMapping("/api/wishlists")
+public class WishlistController {
+    @Autowired
+    WishlistService wService;
+
+    // Get All Data From Table
+    @GetMapping("/{userId}")
+    public ResponseDto index(@PathVariable("userId") int userId) {
+        ResponseDto responseDto = new ResponseDto("200", "Success Find Wishlists", wService.index(userId));
+        return responseDto;
     }
 
     // Save Data To Table
-    public void store()
-    {
-
+    @PostMapping("/store")
+    public ResponseDto store(WishlistDto wDto) {
+        ResponseDto responseDto = new ResponseDto("200", "Success Store Wishlist", wService.store(wDto));
+        return responseDto;
     }
 
     // Update Data To Table
-    public void update()
-    {
+    public void update() {
 
     }
 
     // Delete Data From Table
-    public void destroy()
-    {
-
+    @DeleteMapping("/destroy")
+    public ResponseDto destroy(int wId) {
+        ResponseDto responseDto = new ResponseDto("200", "Success Destroy Wishlist", wService.destroy(wId));
+        return responseDto;
     }
 }
--- src/main/java/com/AltGame/AltGame/Service/WishlistService.java
+++ src/main/java/com/AltGame/AltGame/Service/WishlistService.java
@@ -1,7 +1,42 @@
 package com.AltGame.AltGame.Service;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.AltGame.AltGame.Dto.WishlistDto;
+import com.AltGame.AltGame.Entity.WishlistEntity;
+import com.AltGame.AltGame.Repository.WishlistRepo;
 
 @Service
 public class WishlistService {
+	@Autowired
+	WishlistRepo wishlistRepo;
+
+	public List<WishlistEntity> index(int userId) {
+		return wishlistRepo.findByUserId(userId);
+	}
+
+	public WishlistEntity store(WishlistDto wishlistDto) {
+		WishlistEntity wEntity = new WishlistEntity();
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+		wEntity.setWishlistId(wishlistDto.getWishlistId());
+		wEntity.setUserId(wishlistDto.getUserId());
+		wEntity.setProductId(wishlistDto.getProductId());
+		wEntity.setCreatedAt(timestamp);
+		wEntity.setUpdatedAt(timestamp);
+
+		return wishlistRepo.save(wEntity);
+	}
+
+	public List<WishlistEntity> destroy(int wishlistId) {
+		WishlistEntity wEntity = wishlistRepo.findByWishlistId(wishlistId);
+		if (wEntity != null)
+			wishlistRepo.deleteById(wishlistId);
+		return wishlistRepo.findAll();
+	}
+
 }
