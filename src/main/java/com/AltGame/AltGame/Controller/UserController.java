2b886eef9bf6369f18731a4a0c445d8899a2b62f
Brielt Bella Gracitwo <44756651+EGraci@users.noreply.github.com>
2022-06-29 21:26:56 +0700

    refactoring user controller img to image

--- src/main/java/com/AltGame/AltGame/Controller/UserController.java
+++ src/main/java/com/AltGame/AltGame/Controller/UserController.java
@@ -16,26 +16,6 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/get-image")
-    public @ResponseBody byte[] image_user() throws IOException {
-//        Base64.Decoder decoder = Base64.getDecoder();
-//        String encoded = new String(userService.get_image(userService.authentication().getName()));
-//        return decoder.decode(encoded);
-        String encodeBase64 = new String(userService.get_image(userService.authentication().getName()));
-        byte[] decodedBytes = Base64.getDecoder().decode(encodeBase64);
-        return decodedBytes;
-//        BufferedImage image = null;
-//        BASE64Decoder decoder = new BASE64Decoder();
-//        byte[] imageByte = decoder.decodeBuffer((String) userService.get_image(userService.authentication().getName()));
-//        ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
-//        image = ImageIO.read(bis);
-//        bis.close();
-//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//        ImageIO.write(image, "jpg", baos);
-//        byte[] bytes = baos.toByteArray();
-//        return bytes;
-    }
-
     @GetMapping("/get-user")
     public ResponseDto get_user(){
         Optional<VwUserEntity> vwUserEntity = userService.get_user(userService.authentication().getName());
@@ -45,9 +25,9 @@ public class UserController {
         return new ResponseDto("200", "Success Get Data", vwUserEntity);
     }
     @PostMapping("/update")
-    public ResponseDto update(UserDto userDto,@RequestParam("img") MultipartFile img) throws IOException {
+    public ResponseDto update(UserDto userDto,@RequestParam("image") MultipartFile image) throws IOException {
         userDto.setUsername(userService.authentication().getPrincipal().toString());
-        userDto.setImage(img);
+        userDto.setImage(image);
         if(Objects.nonNull(userService.getUserByUsername(userService.authentication().getPrincipal().toString()))){
             userService.update(userDto);
             return new ResponseDto("200","Success Update");
